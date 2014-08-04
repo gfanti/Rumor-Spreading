54ff06e11137cc530728ea7d077a95615d24cb4f
Giulia Fanti <g.fanti@gmail.com>
2014-08-03 21:45:50 -0500

    utilities library (currently only contains n choose k)

---
+++ python/utilities.py
@@ -0,0 +1,17 @@
+# utilities
+
+
+def nCk(n, k):
+    """
+    A fast way to calculate binomial coefficients by Andrew Dalke (contrib).
+    """
+    if 0 <= k <= n:
+        ntok = 1
+        ktok = 1
+        for t in range(1, min(k, n - k) + 1):
+            ntok *= n
+            ktok *= t
+            n -= 1
+        return ntok // ktok
+    else:
+        return 0
