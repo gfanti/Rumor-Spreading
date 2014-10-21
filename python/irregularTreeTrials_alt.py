43ed95872e0c519ecf794aa28fd8a6a23575c00a
Giulia Fanti <g.fanti@gmail.com>
2014-10-20 21:27:01 -0700

    modified scripts to automatically generate filenames from problem parameters.

--- python/irregularTreeTrials_alt.py
+++ python/irregularTreeTrials_alt.py
@@ -7,31 +7,19 @@ import utilities
 '''Run the irregular tree algorithm'''
 if __name__ == "__main__":
 
-    trials = 10000
-    max_time = 9
+    trials = 500
+    max_time = 6
     
     # Irregular infinite graph
-    xk = np.arange(2,4)
-    pk = (0.5,0.5)
+    xk = np.arange(2,5)
+    pk = (0.25,0.5,0.25)
     dd = 0.5
     ds = np.arange(1.0,4.05,dd)
-    
-    # Regular infinite graph
-    # xk = np.arange(3,4)
-    # pk = (1)
-    # ds = np.array([2])
-    
-    # want to find the best max_infection (i.e., d-1) to minimize pd
-    
-    # max_infection = 100000000
-    # max_infection = min(xk)
-    # max_infection = max(xk)
-    # max_infection = sum([i*j for i,j in zip(xk,pk)])
-    
+        
     num_infected_all = []
     pd_ml_all = []
     
-    for max_infection in ds.tolist():
+    for max_infection in [2]:
 
         print('Checking d_o = ',max_infection+1)
         degrees_rv = stats.rv_discrete(name='rv_discrete', values=(xk, pk))
@@ -40,4 +28,6 @@ if __name__ == "__main__":
         num_infected_all.append(num_infected)
         pd_ml_all.append(pd_ml)
         
-    io.savemat('results/irregular_tree_alt_results',{'pd_ml':np.array(pd_ml_all), 'num_infected':np.array(num_infected_all), 'd_values':ds})
+    xk_str = [str(i) for i in xk]
+    filename = 'results/irregular_tree_alt_results_' + "_".join(xk_str) + '.mat'
+    io.savemat(filename,{'pd_ml':np.array(pd_ml_all), 'num_infected':np.array(num_infected_all), 'd_values':ds})
