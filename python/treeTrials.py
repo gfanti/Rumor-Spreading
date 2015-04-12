1fd4f9583732d8903a95bad75e463186d391d72f
Giulia Fanti <g.fanti@gmail.com>
2015-04-12 15:09:15 -0700

    more trials

--- python/treeTrials.py
+++ python/treeTrials.py
@@ -14,16 +14,16 @@ if __name__ == "__main__":
     # xks = [np.arange(3,5), np.arange(3,6), np.arange(3,20,14)]
     # pks = [(0.5, 0.5), (0.5, 0.25, 0.25), (0.9, 0.1)]
     # max_times = [5, 4, 4]
-    xks = [np.array([2,4]) for i in range(4)]
+    xks = [np.array([3,5]) for i in range(4)]
     # pks = [(0.5, 0.5) for i in range(4)]
     pks = [(0.2*i, 1-0.2*i) for i in range(1,5)]
-    max_times = [6 for i in range(4)]
+    max_times = [4 for i in range(4)]
     
     # xks = [np.arange(2,3)]
     # pks = [(1.0)]
     # max_times = [10]
     # additional_time = 30
-    max_infection = 4 #min(xks) - 1
+    max_infection = 5 #min(xks) - 1
     additional_time = 0
         
     for (xk, pk, max_time) in zip(xks, pks, max_times):
